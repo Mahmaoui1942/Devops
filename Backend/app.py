788888440164a8f7892082d386828cb357533ef8
aynasri <aynasri@a206pc22l.local.isima.fr>
2026-03-11 10:36:00 +0100

    Add Dockerfile and requirements for Flask backend setup

--- Backend/app.py
+++ Backend/app.py
@@ -15,6 +15,6 @@ app.register_blueprint(users_bp)
 app.register_blueprint(games_bp)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
