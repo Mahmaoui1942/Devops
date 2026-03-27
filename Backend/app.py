e9f6fea25310fc3d24be899784b83ff3f18125b3
nasri <nasri-stage@oneex.fr>
2026-03-28 00:01:32 +0100

    Update network policies and configuration for frontend and backend communication; adjust Dockerfile and nginx settings for port exposure

--- Backend/app.py
+++ Backend/app.py
@@ -16,4 +16,4 @@ app.register_blueprint(users_bp)
 app.register_blueprint(games_bp)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
